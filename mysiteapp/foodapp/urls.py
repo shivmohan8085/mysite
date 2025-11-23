87ca3dbd63c9b98b1dddc87364b8fd3fc3bd6a8c
shiv-dev <shiv8085official@gmail.com>
2025-11-23 19:25:20 +0530

    apply class view

--- mysiteapp/foodapp/urls.py
+++ mysiteapp/foodapp/urls.py
@@ -5,10 +5,23 @@ app_name = 'foodapp'
 
 
 urlpatterns = [
-    path("", views.home, name="home"),
-    path('all-items/', views.get_all_data, name='get_all_data'),
-    path('items/<int:id>/', views.detail, name='detail'),
-    path('items/add/', views.create_item, name='create_item'),
-    path('items/<int:id>/update/', views.update_item, name='update_item'),
-    path('items/<int:id>/delete/', views.delete_item, name='delete_item'),
+    # path("", views.home, name="home"),
+    path("", views.HomeView.as_view(), name="home"),
+
+
+    # path('all-items/', views.item_list, name='get_all_data'),
+    path('all-items/', views.FoodItemListView.as_view(), name='get_all_data'),
+
+    # path('items/<int:id>/', views.detail, name='detail'),
+    path('items/<int:id>/', views.FoodDetailView.as_view(), name='detail'),
+
+    # path('items/add/', views.create_item, name='create_item'),
+    path('items/add/', views.ItemCreateView.as_view(), name='create_item'),
+
+    # path('items/<int:id>/update/', views.update_item, name='update_item'),
+    path('items/<int:id>/update/', views.ItemUpdateView.as_view(), name='update_item'),
+
+    # path('items/<int:id>/delete/', views.delete_item, name='delete_item'),
+    path('items/<int:id>/delete/', views.DeleteView.as_view(), name='delete_item'),
+
 ]
--- mysiteapp/foodapp/views.py
+++ mysiteapp/foodapp/views.py
@@ -2,28 +2,46 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import Item
 from .forms import ItemForm
-
 from django.contrib.auth.decorators import login_required
-
-@login_required(login_url="users:login")
-def home(request):
-    return render(request, "foodapp/home.html")
-
+from django.utils.decorators import method_decorator
+from django.views.generic import ListView, DetailView, TemplateView, CreateView , UpdateView , DeleteView
+from django.urls import reverse_lazy
 
 
+# @login_required(login_url="users:login")
+# def home(request):
+#     return render(request, "foodapp/home.html")
 
-# def get_all_data(request):
+@method_decorator(login_required(login_url="users:login"), name='dispatch')
+class HomeView(TemplateView):
+    template_name = "foodapp/home.html"
+
+
+#----------------------------------
+
+
+# def item_list(request):
 #     item_list = Item.objects.all()
 #     context= {
 #         'item_list':item_list
 #     }
 #     return render(request, "foodapp/display-data.html", context)
 
-@login_required(login_url="users:login")
-def get_all_data(request):
-    """Display all items"""
-    item_list = Item.objects.all()
-    return render(request, "foodapp/display-data.html", {"item_list": item_list})
+# @login_required(login_url="users:login")
+# def item_list(request):
+#     """Display all items"""
+#     item_list = Item.objects.all()
+#     return render(request, "foodapp/display-data.html", {"item_list": item_list})
+
+@method_decorator(login_required(login_url="users:login"), name='dispatch')
+class FoodItemListView(ListView):
+    model = Item
+    template_name = "foodapp/display-data.html"
+    context_object_name= "item_list"      # by default ->  Item.objects.all()
+
+
+
+#----------------------
 
 # def detail(request, id):
 #     item_detail = Item.objects.get(id=id)
@@ -32,31 +50,57 @@ def get_all_data(request):
 #     }
 #     return render(request, "foodapp/details.html", context)
 
-@login_required(login_url="users:login")
-def detail(request, id):
-    """Display a specific item's details"""
-    item_detail = get_object_or_404(Item, id=id)
-    return render(request, "foodapp/details.html", {"item_detail": item_detail})
+# @login_required(login_url="users:login")
+# def detail(request, id):
+#     """Display a specific item's details"""
+#     item_detail = get_object_or_404(Item, id=id)
+#     return render(request, "foodapp/details.html", {"item_detail": item_detail})
+
+
+@method_decorator(login_required(login_url="users:login"), name='dispatch')
+class FoodDetailView(DetailView):
+    model= Item
+    template_name = 'foodapp/details.html'
+    context_object_name = 'item_detail'
+    pk_url_kwarg = "id" 
+
+
+#--------------------------------------------------
 
 
 
-@login_required(login_url="users:login")
-def create_item(request):
-    if request.method == 'POST':
-        form = ItemForm(request.POST)
-        if form.is_valid():
-            form.save()
-            print("Item created successfully")
-            return redirect('foodapp:get_all_data')
-    else:
-        form = ItemForm()
-
-    context = {
-        'form': form
-    }
-    return render(request, "foodapp/item-form.html", context)
 
 
+# @login_required(login_url="users:login")
+# def create_item(request):
+#     if request.method == 'POST':
+#         form = ItemForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             print("Item created successfully")
+#             return redirect('foodapp:get_all_data')
+#     else:
+#         form = ItemForm()
+
+#     context = {
+#         'form': form
+#     }
+#     return render(request, "foodapp/item-form.html", context)
+
+@method_decorator(login_required(login_url="users:login"), name='dispatch')
+class ItemCreateView(CreateView):
+    model = Item
+    form_class = ItemForm
+    template_name = "foodapp/item-form.html"
+    success_url = reverse_lazy("foodapp:get_all_data")
+
+    def form_valid(self, form):
+        print("Item created successfully")
+        return super().form_valid(form)
+
+
+
+#----------------------------------------------
 
 # def update_item(request, id):
 #     item_detail = Item.objects.get(id=id)
@@ -70,21 +114,38 @@ def create_item(request):
 #     }
 #     return render(request, "foodapp/item-form.html", context)
 
-@login_required(login_url="users:login")
-def update_item(request, id):
-    """Update an existing item"""
-    item_detail = get_object_or_404(Item, id=id)
+# @login_required(login_url="users:login")
+# def update_item(request, id):
+#     """Update an existing item"""
+#     item_detail = get_object_or_404(Item, id=id)
 
-    if request.method == 'POST':
-        form = ItemForm(request.POST, instance=item_detail)
-        if form.is_valid():
-            form.save()
-            print("Item updated successfully")
-            return redirect('foodapp:get_all_data')
-    else:
-        form = ItemForm(instance=item_detail)
+#     if request.method == 'POST':
+#         form = ItemForm(request.POST, instance=item_detail)
+#         if form.is_valid():
+#             form.save()
+#             print("Item updated successfully")
+#             return redirect('foodapp:get_all_data')
+#     else:
+#         form = ItemForm(instance=item_detail)
 
-    return render(request, "foodapp/item-form.html", {"form": form})
+#     return render(request, "foodapp/item-form.html", {"form": form})
+
+@method_decorator(login_required(login_url="users:login"), name='dispatch')
+class ItemUpdateView(UpdateView):
+    model = Item
+    form_class = ItemForm
+    template_name = "foodapp/item-form.html"
+    success_url = reverse_lazy("foodapp:get_all_data")
+    context_object_name = "item"
+    pk_url_kwarg = "id"   
+
+    def form_valid(self, form):
+        print("Item updated successfully")
+        return super().form_valid(form)
+
+
+
+#---------------
 
 
 # def delete_item(request, id):
@@ -93,11 +154,21 @@ def update_item(request, id):
 #     print("Item deleted successfully")
 #     return redirect('foodapp:get_all_data')
 
-@login_required(login_url="users:login")
-def delete_item(request, id):
-    """Delete an item"""
-    item = get_object_or_404(Item, id=id)
-    item.delete()
-    print("Item deleted successfully")
-    return redirect('foodapp:get_all_data')
+# @login_required(login_url="users:login")
+# def delete_item(request, id):
+#     """Delete an item"""
+#     item = get_object_or_404(Item, id=id)
+#     item.delete()
+#     print("Item deleted successfully")
+#     return redirect('foodapp:get_all_data')
 
+
+@method_decorator(login_required(login_url="users:login"), name='dispatch')
+class ItemDeleteView(DeleteView):
+    model = Item
+    template_name = "foodapp/item-confirm-delete.html"  # confirmation page
+    success_url = reverse_lazy("foodapp:get_all_data")
+    pk_url_kwarg = "id"  
+    def delete(self, request, *args, **kwargs):
+        print("Item deleted successfully")
+        return super().delete(request, *args, **kwargs)
